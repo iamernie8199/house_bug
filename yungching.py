05eafde064bef60879d40771e41e5def7d8af13b
iamernie8199 <iamernie8199@gmail.com>
2023-03-03 00:19:02 +0800

    永慶基本功能

---
+++ yungching.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+if __name__ == '__main__':
+    region = '台北市'
+    region_sub = ''
+    budget_start = '1500'
+    budget_end = '2500'
+    age_start = ''
+    age_end = '30'
+    yungching_url = 'https://buy.yungching.com.tw'
+    url = f'{yungching_url}/region/{region}-{region_sub}_c/{budget_start}-{budget_end}_price/{age_start}-{age_end}_age/'
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(url)
+
+    url = driver.find_element('xpath', "//a[text()='最末頁']").get_attribute('href')
+    max_page = int(url.split('=')[-1])
+    url = url.split('=')[0] + '='
+    house_list = []
+    urls = [url + str(i + 1) for i in range(max_page)]
+
+    for u in urls:
+        driver.get(u)
+
+        list_items = driver.find_elements('class name', 'm-list-item')
+        for li in list_items:
+            info = li.find_element('class name', 'item-info-detail')
+            item = {
+                'title': li.find_element('tag name', 'h3').text,
+                'price': float(li.find_element('class name', 'price-num').text.replace(',', '')),
+                'price_sub': li.find_element('class name', 'sub-price').text,
+                'address': li.find_element('tag name', 'span').text,
+                'type': info.find_element('tag name', 'li').text,
+                'age': float(info.find_elements('tag name', 'li')[1].text.replace('年', '')),
+                'floor': info.find_elements('tag name', 'li')[2].text,
+                '地': info.find_elements('tag name', 'li')[3].text,
+                '實': info.find_elements('tag name', 'li')[4].text,
+                '建': info.find_elements('tag name', 'li')[5].text,
+                '格局': info.find_elements('tag name', 'li')[6].text,
+                'url': li.find_element('tag name', 'a').get_attribute('href')
+            }
+            house_list.append(item)
+    df = pd.DataFrame.from_dict(house_list)
